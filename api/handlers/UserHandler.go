f356a47909316b9c63a96b2f571582e7f8359376
Luk <lucapierini03@gmail.com>
2024-12-27 17:30:34 -0300

    Avances de handler y creacion de función de buscar por email

--- api/handlers/UserHandler.go
+++ api/handlers/UserHandler.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+
 	"github.com/gin-gonic/gin"
+	"github.com/lucapierini/project-go-task_manager/dto"
 	"github.com/lucapierini/project-go-task_manager/services"
 )
 
@@ -15,6 +17,19 @@ func NewUserHandler(userService services.UserInterface) *UserHandler{
 	}
 }
 
-func (h *UserHandler) RegisterUser(c *gin.Context){
-	users, err := 
+func (h *UserHandler) Register(c *gin.Context){
+	var userDto dto.UserDto
+
+	if c.Bind(&userDto) != nil {
+		c.JSON(400, gin.H{"error": "Invalid data"})
+		return
+	}
+
+	user, err := h.userService.RegisterUser(userDto)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, user)
 }
--- api/services/UserService.go
+++ api/services/UserService.go
@@ -1,129 +1,139 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/lucapierini/project-go-task_manager/config"
 	"github.com/lucapierini/project-go-task_manager/dto"
 	"github.com/lucapierini/project-go-task_manager/models"
 	"github.com/lucapierini/project-go-task_manager/utils"
 	"golang.org/x/crypto/bcrypt"
-	"errors"
 )
 
 type UserInterface interface {
 	RegisterUser(userDto dto.UserDto) (*models.User, error)
-    LoginUser(userDto dto.UserDto) (*models.User, error)
-    GetUser(id uint) (*models.User, error)
-    ListUsers() ([]models.User, error)
-    UpdateUser(id uint, userDto dto.UserDto) (*models.User, error)
-    DeleteUser(id uint) error
+	LoginUser(userDto dto.UserDto) (*models.User, error)
+	GetUserById(id uint) (*models.User, error)
+	GetUserByEmail(email string) (*models.User, error)
+	ListUsers() ([]models.User, error)
+	UpdateUser(id uint, userDto dto.UserDto) (*models.User, error)
+	DeleteUser(id uint) error
 }
 
 func RegisterUser(userDto dto.UserDto) (*models.User, error) {
-// Check if email already exists
-var existingUser models.User
-if result := config.DB.Where("email = ?", userDto.Email).First(&existingUser); result.Error == nil {
-	return nil, errors.New("email already registered")
-}
+	// Check if email already exists
+	var existingUser models.User
+	if result := config.DB.Where("email = ?", userDto.Email).First(&existingUser); result.Error == nil {
+		return nil, errors.New("email already registered")
+	}
 
-// Hash password
-hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), bcrypt.DefaultCost)
-if err != nil {
-	return nil, err
-}
-
-// Create user
-user := models.User{
-	Username: userDto.Username,
-	Email:    userDto.Email,
-	Password: string(hashedPassword),
-}
-
-// Add roles if specified
-if len(userDto.RoleIds) > 0 {
-	var roles []models.Role
-	if err := config.DB.Find(&roles, userDto.RoleIds).Error; err != nil {
+	// Hash password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), bcrypt.DefaultCost)
+	if err != nil {
 		return nil, err
 	}
-	user.Roles = roles
+
+	// Create user
+	user := models.User{
+		Username: userDto.Username,
+		Email:    userDto.Email,
+		Password: string(hashedPassword),
+	}
+
+	// Add roles if specified
+	if len(userDto.RoleIds) > 0 {
+		var roles []models.Role
+		if err := config.DB.Find(&roles, userDto.RoleIds).Error; err != nil {
+			return nil, err
+		}
+		user.Roles = roles
+	}
+
+	if err := config.DB.Create(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
-if err := config.DB.Create(&user).Error; err != nil {
-	return nil, err
+func GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := config.DB.Preload("Roles").Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
-return &user, nil}
-
-func GetUser(id uint) (*models.User, error) {
+func GetUserById(id uint) (*models.User, error) {
     var user models.User
-    if err := config.DB.Preload("Roles").First(&user, id).Error; err != nil {
+    if err := config.DB.Preload("Roles").Where("id = ?", id).First(&user).Error; err != nil {
         return nil, err
     }
     return &user, nil
 }
 
 func UpdateUser(id uint, userDto dto.UserDto) (*models.User, error) {
-    user, err := GetUser(id)
-    if err != nil {
-        return nil, err
-    }
+	user, err := GetUserById(id)
+	if err != nil {
+		return nil, err
+	}
 
-    user.Username = userDto.Username
-    user.Email = userDto.Email
+	user.Username = userDto.Username
+	user.Email = userDto.Email
 
-    if userDto.Password != "" {
-        hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), bcrypt.DefaultCost)
-        if err != nil {
-            return nil, err
-        }
-        user.Password = string(hashedPassword)
-    }
+	if userDto.Password != "" {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return nil, err
+		}
+		user.Password = string(hashedPassword)
+	}
 
-    // Update roles if specified
-    if len(userDto.RoleIds) > 0 {
-        var roles []models.Role
-        if err := config.DB.Find(&roles, userDto.RoleIds).Error; err != nil {
-            return nil, err
-        }
-        user.Roles = roles
-    }
+	// Update roles if specified
+	if len(userDto.RoleIds) > 0 {
+		var roles []models.Role
+		if err := config.DB.Find(&roles, userDto.RoleIds).Error; err != nil {
+			return nil, err
+		}
+		user.Roles = roles
+	}
 
-    if err := config.DB.Save(user).Error; err != nil {
-        return nil, err
-    }
+	if err := config.DB.Save(user).Error; err != nil {
+		return nil, err
+	}
 
-    return user, nil
+	return user, nil
 }
 
-
 func DeleteUser(id uint) error {
-    if err := config.DB.Delete(&models.User{}, id).Error; err != nil {
-        return err
-    }
-    return nil
+	if err := config.DB.Delete(&models.User{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func ListUsers() ([]models.User, error) {
-    var users []models.User
-    if err := config.DB.Preload("Roles").Find(&users).Error; err != nil {
-        return nil, err
-    }
-    return users, nil
+	var users []models.User
+	if err := config.DB.Preload("Roles").Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 func LoginUser(loginDto dto.LoginDto) (string, error) {
-    var user models.User
-    if err := config.DB.Where("email = ?", loginDto.Email).First(&user).Error; err != nil {
-        return "", errors.New("invalid credentials")
-    }
+	var user models.User
+	if err := config.DB.Where("email = ?", loginDto.Email).First(&user).Error; err != nil {
+		return "", errors.New("invalid credentials")
+	}
 
-    if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginDto.Password)); err != nil {
-        return "", errors.New("invalid credentials")
-    }
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginDto.Password)); err != nil {
+		return "", errors.New("invalid credentials")
+	}
 
-    token, err := utils.GenerateToken(user)
-    if err != nil {
-        return "", err
-    }
+	token, err := utils.GenerateToken(user)
+	if err != nil {
+		return "", err
+	}
 
-    return token, nil
+	return token, nil
 }
